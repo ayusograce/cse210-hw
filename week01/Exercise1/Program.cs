bfca91faf010ecc202d8207f505bb0e27327fdff
ayusograce <adriayusof@gmail.com>
2025-01-09 18:59:45 -0500

    Exercise 1 completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello World! This is the Exercise1 Project.\n");
+
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lname}, {fname} {lname}.");
     }
 }
