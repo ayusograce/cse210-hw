2464adf41ddbf6274f7e9b11bb380fda44e87b41
ayusograce <adriayusof@gmail.com>
2025-01-14 19:17:25 -0500

    learning activity week 2 completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2014;
+        job1._endYear = 2016;
+
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Web developer";
+        job2._startYear = 2017;
+        job2._endYear = 2022;
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        resume1.DisplayResume();
     }
 }
