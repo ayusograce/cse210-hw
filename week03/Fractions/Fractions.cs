442c6262a129dfdf10ff62f9023f6ecb6813f62b
ayusograce <adriayusof@gmail.com>
2025-01-20 16:52:30 -0500

    Learning activities week 2 completed

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,56 @@
+public class Fractions
+{
+    private int _top;
+    private int _bottom;
+
+    //CONSTRUCTORS
+    public Fractions()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fractions(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fractions(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    //GETTERS AND SETTERS
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    //METHODS
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fractions fraction1 = new Fractions();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        fraction1.SetTop(5);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        fraction1.SetTop(3);
+        fraction1.SetBottom(4);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        fraction1.SetTop(1);
+        fraction1.SetBottom(3);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
     }
+
+    
 }
