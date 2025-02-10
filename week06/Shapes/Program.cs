9439bf456573a6c36360b33e41b84adeba03e103
ayusograce <adriayusof@gmail.com>
2025-02-10 16:07:58 -0500

    learning activity completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square sq = new Square("black", 5);
+        Rectangle rec = new Rectangle("pink", 3.5, 1);
+        Circle cir = new Circle("white", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sq);
+        shapes.Add(rec);
+        shapes.Add(cir);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"\nThe color is {color} and the area is {area}");
+        }
+
     }
 }
